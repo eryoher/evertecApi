94fa43310705d391a2145f2f5dafe20a6d3fc800
eryoher <eryoher@gmail.com>
2020-04-18 22:06:55 -0500

    config server

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    apps : [{
+      name: 'Evertec - API',
+      script: 'server/server.js',
+      instances: 1,
+      autorestart: true,
+      watch: true,
+      max_memory_restart: '512M',
+      log_date_format: 'YYYY-MM-DD HH:mm:ss SSS',
+      env: {
+        NODE_ENV: 'dev'
+      },
+      env_stg: {
+        NODE_ENV: 'stg'
+      }
+    }]
+};
